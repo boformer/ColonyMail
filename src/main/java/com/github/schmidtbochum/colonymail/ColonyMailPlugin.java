058d0c1d75391bff6291f9d44afa523dc377f61b
schmidtbochum <colony@schmidt-bochum.eu>
2014-07-23 02:42:33 +0200

    Added arg handler and command class

--- src/main/java/com/github/schmidtbochum/colonymail/ColonyMailPlugin.java
+++ src/main/java/com/github/schmidtbochum/colonymail/ColonyMailPlugin.java
@@ -1,21 +1,27 @@
-package com.github.schmidtbochum.colonymail;
+ package com.github.schmidtbochum.colonymail;
 
 import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
+import se.ranzdo.bukkit.methodcommand.CommandHandler;
+
 import com.github.schmidtbochum.colonydata.ColonyDataPlugin;
-import com.github.schmidtbochum.colonydata.data.CPlayerGroup;
+import com.github.schmidtbochum.colonydata.command.CPlayerArgumentHandler;
+import com.github.schmidtbochum.colonydata.data.CMailGroup;
 import com.github.schmidtbochum.colonydata.data.CMail;
 import com.github.schmidtbochum.colonydata.data.CPlayer;
 import com.github.schmidtbochum.colonydata.data.DataManager;
+import com.github.schmidtbochum.colonymail.command.MailCommand;
 import com.github.schmidtbochum.util.MessageManager;
 
 public class ColonyMailPlugin extends JavaPlugin
 {
-	DataManager d;
-	MessageManager m;
+	private DataManager d;
+	private MessageManager m;
+	private CommandHandler commandHandler;
 
 	
 	public void onEnable() 
@@ -26,17 +32,26 @@ public class ColonyMailPlugin extends JavaPlugin
 		
 		m = new MessageManager(this, Locale.ENGLISH, ResourceBundle.getBundle("messages", Locale.ENGLISH));
 		
-		d = ((ColonyDataPlugin) getServer().getPluginManager().getPlugin("ColonyData")).getDataManager();
+		ColonyDataPlugin dataPlugin = (ColonyDataPlugin) getServer().getPluginManager().getPlugin("ColonyData");
+		
+		d = dataPlugin.getDataManager();
+		
+		commandHandler = new CommandHandler(this);
+		
+		commandHandler.registerArgumentHandler(CPlayer.class, new CPlayerArgumentHandler(dataPlugin));
+		
+		commandHandler.registerCommands(new MailCommand(this));
+		
 	}
 	
-	public void sendMail(CPlayer sender, CPlayer receipient, CPlayerGroup playerGroup, String message)
+	public void sendMail(CPlayer sender, CPlayer receipient, CMailGroup playerGroup, String message)
 	{
 		CMail mail = d.createEntityBean(CMail.class);
 		
 		mail.setDate(new Date());
 		mail.setSender(sender);
 		mail.setReceipient(receipient);
-		mail.setPlayerGroup(playerGroup);
+		mail.setMailGroup(playerGroup);
 		mail.setMessage(message);
 		
 		d.save(mail);
---
+++ src/main/java/com/github/schmidtbochum/colonymail/command/MailCommand.java
@@ -0,0 +1,14 @@
+package com.github.schmidtbochum.colonymail.command;
+
+import com.github.schmidtbochum.colonymail.ColonyMailPlugin;
+
+public class MailCommand
+{
+	private ColonyMailPlugin plugin;
+
+	public MailCommand(ColonyMailPlugin plugin)
+	{
+		this.plugin = plugin;
+	}
+
+}
