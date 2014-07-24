de9e8c6ed8f8f88aa89a7bfca4ba339c70b689b5
schmidtbochum <colony@schmidt-bochum.eu>
2014-07-24 14:44:50 +0200

    added method body for /mail send command

--- src/main/java/com/github/schmidtbochum/colonymail/command/MailCommand.java
+++ src/main/java/com/github/schmidtbochum/colonymail/command/MailCommand.java
@@ -1,5 +1,12 @@
 package com.github.schmidtbochum.colonymail.command;
 
+import org.bukkit.command.CommandSender;
+
+import se.ranzdo.bukkit.methodcommand.Arg;
+import se.ranzdo.bukkit.methodcommand.Command;
+import se.ranzdo.bukkit.methodcommand.Wildcard;
+
+import com.github.schmidtbochum.colonydata.data.CPlayer;
 import com.github.schmidtbochum.colonymail.ColonyMailPlugin;
 
 public class MailCommand
@@ -11,4 +18,20 @@ public class MailCommand
 		this.plugin = plugin;
 	}
 
+	@Command(
+		identifier="test", //The identifier is the way the command will be accessed ingame, this command will be accessed with /test
+		description="This is a test command", //Writing the description of the command here makes it easier to generate help messages.
+		onlyPlayers = false, //If the command can be executed by a player or not.
+		permissions = {"coremail.command.mail.send"} //What permission nodes the player must have to execute this command
+	)
+	//mail send <player> <msg>
+	public void sendSelf(
+	    CommandSender sender,
+	    @Arg(name="player") CPlayer receipient,
+	    @Wildcard @Arg(name="msg") String message
+	) 
+	{
+	    //TODO
+		sender.sendMessage("to " + receipient.getName() + ": " + message);
+	}
 }
