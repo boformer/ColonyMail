4909e9a7ca166a2b39de7dff016f11dd82d8f144
schmidtbochum <colony@schmidt-bochum.eu>
2014-07-27 22:16:45 +0200

    added paging list for mails

---
+++ src/main/java/com/github/schmidtbochum/colonymail/CMailPagingList.java
@@ -0,0 +1,31 @@
+package com.github.schmidtbochum.colonymail;
+
+import java.util.List;
+import org.bukkit.command.CommandSender;
+
+import com.github.schmidtbochum.colonydata.data.CMail;
+import com.github.schmidtbochum.util.MessageManager;
+import com.github.schmidtbochum.util.SimplePagingList;
+
+public class CMailPagingList extends SimplePagingList<CMail>
+{
+	
+	
+	public CMailPagingList(List<CMail> elementList, MessageManager m, int expirationMinutes, String pagingCommand)
+	{
+		super(elementList, m, expirationMinutes, pagingCommand, "page_mail_header", "page_mail_no_elements");
+	}
+
+	@Override
+	public void sendElement(CommandSender receipient, CMail element)
+	{
+		if(element.getMailGroup() == null) 
+		{
+			m.sendMessage("page_mail_template", receipient, element.getSender().getName(), element.getMessage());
+		}
+		else
+		{
+			m.sendMessage("page_mail_group_template", receipient, element.getMailGroup().getName(), element.getSender().getName(), element.getMessage());
+		}
+	}
+}
